79f33130bdb876ba3d502de6a7b196354cf3a733
SarahEssaam <sarah3ssam@gmail.com>
2016-10-27 22:06:43 +0200

    Update joystick.cpp
    omit QObject ,signals, and slots,

--- joystick.cpp
+++ joystick.cpp
@@ -1,32 +1,37 @@
-#include "joystick.h"
 
-Joystick::Joystick(QObject *parent) : QObject(parent)
+#include mysfml, mysdl, mode1, mode2, mode3 .h
+
+class Joystick 
 {
+private:
+    int activeMode = 0;
+    arrayModes[];
+    //x,y,povx,povy,z,buttons[],..
+    //create pointer to the library we will use(or object from)
+    //MySFML / MySDL myLibrary; 
+    
 
-    QObject::connect(this,SIGNAL(//initiate mode1 signal),object from class mode1,SLOT(Do mode1 slot));
-    QObject::connect(this,SIGNAL(//initiate mode2 signal),object from class mode2,SLOT(Do mode2 slot));
-    QObject::connect(this,SIGNAL(//initiate mode3 signal),object from class mode3,SLOT(Do mode3 slot));
-    QObject::connect(this,SIGNAL(timeout()),this,SLOT(updateModes)));
-
-
+Joystick
+{
+    Mode* arrayModes[MAX];
+    arrayModes[0] = new Mode1();
+    arrayModes[1] = new Mode2();
+    arrayModes[2] = new Mode3();
     //myLibrary = new MySFML or MySDL
     //myLibrary will be sent as a parameter to all the modes
-
-
 }
 
 void updateModes(){
     //sf::joystick::update()
     //if(sf::joystick::isconnected)
 
-    //check on general functions' (that are not tied to a mode)
-    //like tilt, rotate, camera, stablization, (sensitivity?), (fe 7aga tania?)
-
-    //loop on all modes' buttons and check if isButtonPressed set activeMode=mode Pressed
-    //emit signal for the active mode
-
-    //recieve the values sent from Do mode
+    //the common functions will ither be implemented here or in Mode class
+    
+    //loop on all modes' buttons and check if isButtonPressed(0,i) set activeMode=mode Pressed
+   
+    //arrayModes[activeMode]->do_x();
+    //recieve the values sent back from do_x()
     //check for the change in those values
     //send them via tcp
-
 }
+};
