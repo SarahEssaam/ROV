3c83bd1a391df552ad0fcb55f56f9166b70cac97
SarahEssaam <sarah3ssam@gmail.com>
2016-10-31 00:24:15 +0200

    Update mode2.cpp

--- mode2.cpp
+++ mode2.cpp
@@ -1,20 +1,30 @@
-#include "mode2.h"
+#include "mode.h"
+#define STEP 0.25
 
-Mode2 : public Mode
+//driving mode
+
+class Mode1 : public Mode
 {
-
-}
-
-void do_x(){
-}
-
-        else if(mode==2)
-        {
-            manX=sf::Joystick::getAxisPosition(0,sf::Joystick::X);
-            manY=sf::Joystick::getAxisPosition(0,sf::Joystick::Y);
-            x=sf::Joystick::getAxisPosition(0,sf::Joystick::PovX);
-            y=sf::Joystick::getAxisPosition(0,sf::Joystick::PovY);
             
-        }
-
-    }
+void do_x(){
+            manX=myLibrary->getAxisPosition(0,X);
+            manY=myLibrary->getAxisPosition(0,Y);
+            
+            // neglect small changes in x and y
+             if(error_x())
+             manX=0;
+             if(error_y())
+             manY=0;
+                             
+            x=myLibrary->getAxisPosition(0,PovX)*STEP;
+            y=myLibrary->getAxisPosition(0,PovY)*STEP;    
+            
+            // neglect small changes in manx &y
+            if(error_PovX)
+            x=0;
+            if(error_PovY)
+            y=0;
+     
+           }
+           
+};
