352c5e3d8af10171fb775adff9a38afef1aa13e5
SarahEssaam <sarah3ssam@gmail.com>
2016-10-30 22:40:04 +0200

    Update mode2.cpp

--- mode2.cpp
+++ mode2.cpp
@@ -7,3 +7,14 @@ Mode2 : public Mode
 
 void do_x(){
 }
+
+        else if(mode==2)
+        {
+            manX=sf::Joystick::getAxisPosition(0,sf::Joystick::X);
+            manY=sf::Joystick::getAxisPosition(0,sf::Joystick::Y);
+            x=sf::Joystick::getAxisPosition(0,sf::Joystick::PovX);
+            y=sf::Joystick::getAxisPosition(0,sf::Joystick::PovY);
+            
+        }
+
+    }
