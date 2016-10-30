2218e45b21f8957348b2f3d10aa42fb42a7a3082
SarahEssaam <sarah3ssam@gmail.com>
2016-10-30 22:39:26 +0200

    Update mode1.cpp

--- mode1.cpp
+++ mode1.cpp
@@ -1,5 +1,8 @@
 #include "mode.h"
 
+
+//driving mode
+
 Mode1 : public Mode
 {
 
@@ -7,3 +10,7 @@ Mode1 : public Mode
 
 void do_x(){
 }
+x=sf::Joystick::getAxisPosition(0,sf::Joystick::X);
+            y=sf::Joystick::getAxisPosition(0,sf::Joystick::Y);
+            manX=sf::Joystick::getAxisPosition(0,sf::Joystick::PovX);
+            manY=sf::Joystick::getAxisPosition(0,sf::Joystick::PovY);
