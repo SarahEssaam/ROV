96a93def70dbb78fe02ac85cd76a04e2e97a5108
SarahEssaam <sarah3ssam@gmail.com>
2016-10-30 22:42:05 +0200

    Update mode3.cpp

--- mode3.cpp
+++ mode3.cpp
@@ -7,3 +7,9 @@ Mode3 : public Mode
 
 void do_x(){
 }
+ while(sf::Joystick::isButtonPressed(0, tilt) && states[tilt])
+    {
+       t=sf::Joystick::getAxisPosition(0,sf::Joystick::Y);
+    }
+
+//case rotate..
