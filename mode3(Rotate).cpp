1e0410dd0432bdb84d6334904b2d6f66e56b8904
SarahEssaam <sarah3ssam@gmail.com>
2016-11-01 14:36:49 +0200

    Update mode3(Rotate).cpp

--- mode3(Rotate).cpp
+++ mode3(Rotate).cpp
@@ -5,8 +5,8 @@ class Rotate : public Mode
 
 void do_x(MyJoysickLibrary* myLibrary,QMap* axis){
 
- axis["tilt"] = myLibrary->Get_y();
- axis["rotate"] = myLibrary->Get_r();
+ *axis["tilt"] = myLibrary->Get_y();
+ *axis["rotate"] = myLibrary->Get_r();
 
 }
 };
