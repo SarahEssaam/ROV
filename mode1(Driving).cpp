039e7091a7ad56ed6adacd12897cb9cb8ab12188
SarahEssaam <sarah3ssam@gmail.com>
2016-11-01 14:02:13 +0200

    Update and rename mode1.cpp to mode1(Driving).cpp

---
+++ mode1(Driving).cpp
@@ -0,0 +1,20 @@
+#include "mode.h"
+#define STEP 0.25
+
+//driving mode
+
+class Driving : public Mode
+{
+            
+void do_x(MyJoysickLibrary* myLibrary,QMap* axis){
+          
+            axis["x"]=myLibrary->Get_x();
+            axis["y"]=myLibrary->Get_y();
+            
+                             
+            axis["manX"]=myLibrary->Get_povx()*STEP;
+            axis["manY"]=myLibrary->Get_povy()*STEP;    
+            
+           }
+           
+};
