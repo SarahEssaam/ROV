51a2ed02f1b164e6c4222d933eaa4e8041e40615
SarahEssaam <sarah3ssam@gmail.com>
2016-11-01 14:04:55 +0200

    Update mode.cpp

--- mode.cpp
+++ mode.cpp
@@ -1,30 +1,12 @@
 //parent class to all modes
+#include "joystick.h"
 #define MIN_X 1
 #define MIN_POV 5
 
 
 class Mode{
 public:
-virtual void do_x(){}
-bool error_x(){
-  if((myLibrary->getAxisPosition(0,X)<MIN_X)&&(myLibrary->getAxisPosition(0,X)>-1*MIN_X))
-  return true;
-}
-  bool error_y(){
-  if((myLibrary->getAxisPosition(0,y)<MIN_X)&&(myLibrary->getAxisPosition(0,y)>-1*MIN_X))
-  return true;
-}
-  bool error_PovX(){
-  if((myLibrary->getAxisPosition(0,PovX)<MIN_POV)&&(myLibrary->getAxisPosition(0,PovX)>-1*MIN_POV))
-  return true;
-}
-  bool error_PovY(){
-  if((myLibrary->getAxisPosition(0,PovY)<MIN_POV)&&(myLibrary->getAxisPosition(0,PovY)>-1*MIN_POV))
-  return true;
-}
-  
-  
-  
-  
+virtual void do_x(MyJoystickLibrary*,Qmap*){}
+
 
 };
