e95c617be298f4ab1c6586a3e280dacc7c335ed9
SarahEssaam <sarah3ssam@gmail.com>
2016-11-01 14:35:52 +0200

    Update mode1(Drive).cpp

--- mode1(Drive).cpp
+++ mode1(Drive).cpp
@@ -8,14 +8,14 @@ class Drive : public Mode
             
 void do_x(MyJoysickLibrary* myLibrary,QMap* axis){
           
-   /*  
-            axis["x"]=myLibrary->Get_x();
-            axis["y"]=myLibrary->Get_y();
+    
+            *axis["x"]=myLibrary->Get_x();
+            *axis["y"]=myLibrary->Get_y();
             
                              
-            axis["manX"]=myLibrary->Get_povx()*STEP;
-            axis["manY"]=myLibrary->Get_povy()*STEP;    
-     */       
+            *axis["manX"]=myLibrary->Get_povx()*STEP;
+            *axis["manY"]=myLibrary->Get_povy()*STEP;    
+            
            }
            
 };
