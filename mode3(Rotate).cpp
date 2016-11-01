810eb0345ba821d51dc68adb113d52df544df627
SarahEssaam <sarah3ssam@gmail.com>
2016-11-01 14:01:28 +0200

    Update and rename mode3.cpp to mode3(Rotate).cpp

---
+++ mode3(Rotate).cpp
@@ -0,0 +1,12 @@
+#include "mode.h"
+
+class Rotate : public Mode
+{
+
+void do_x(MyJoysickLibrary* myLibrary,QMap* axis){
+
+ axis["tilt"] = myLibrary->Get_y();
+ axis["rotate"] = myLibrary->Get_r();
+
+}
+};
