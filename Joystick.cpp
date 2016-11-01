5b902c71749ab4db1f1258da836e7a7f0846b952
SarahEssaam <sarah3ssam@gmail.com>
2016-11-01 14:48:33 +0200

    Update Joystick.cpp

--- Joystick.cpp
+++ Joystick.cpp
@@ -2,11 +2,15 @@
 
 Joystick(){
   my_library = new MyJoystickLibrary();
+  
   mode[DRIVE] = new Drive();
   mode[MANIP] = new Manip();
   mode[ROTATE] = new Rotate();
   active_mode = mode[DRIVE];
   previous_mode = mode[DRIVE];
+  
+  init_axis();
+  
   timer = new QTimer();
   QObject::connect(this,SIGNAL(timeOut()),this,SLOT(update_states()));
 }
@@ -15,12 +19,12 @@ Joystick(){
 update_states{
   update_modes();
   if(active_mode!=previous_mode){
-   reset_variables();
+   reset_axis();
    previous_mode = active_mode;
   }
-//  active_mode->do_x(myLibrary,&current_axis);
+  active_mode->do_x(myLibrary,&current_axis);
   update_buttons();
-//  tcp_object.send(&current_axis);
+  tcp_object.send(current_axis);
 }
 
 
@@ -30,10 +34,30 @@ void update_modes(){
 void update_buttons(){
 }
 
-reset_variables(){
+void reset_axis(){
   /*
   QMap<QString, int>::iterator i;
   for (i = current_axis.begin(); i != current_axis.end(); ++i)
     i = 0;
     */
+}
+
+//msh sure m7taga a7gz fehom mkan kda wla l2 
+void init_axis(){
+  current_axis["x"] = 0;
+  current_axis["y"] = 0;
+  current_axis["z"] = 0;
+  current_axis["manX"] = 0;
+  current_axis["manY"] = 0;
+  current_axis["camera"] = 90;
+  current_axis["tilt"] = 0;
+  current_axis["rotate"] = 0;
+}
+
+void init_modes(){
+  mode[DRIVE] = new Drive();
+  mode[MANIP] = new Manip();
+  mode[ROTATE] = new Rotate();
+  active_mode = mode[DRIVE];
+  previous_mode = mode[DRIVE];
 }
