38e9c11aa0cf692d15fc3fb4e9cdebc6274e56b3
SarahEssaam <sarah3ssam@gmail.com>
2016-11-01 14:18:41 +0200

    Update and rename mode1(Driving).cpp to mode1(Drive).cpp

--- mode1(Drive).cpp
+++ mode1(Drive).cpp
@@ -3,18 +3,19 @@
 
 //driving mode
 
-class Driving : public Mode
+class Drive : public Mode
 {
             
 void do_x(MyJoysickLibrary* myLibrary,QMap* axis){
           
+   /*  
             axis["x"]=myLibrary->Get_x();
             axis["y"]=myLibrary->Get_y();
             
                              
             axis["manX"]=myLibrary->Get_povx()*STEP;
             axis["manY"]=myLibrary->Get_povy()*STEP;    
-            
+     */       
            }
            
 };
